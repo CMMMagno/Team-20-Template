c18d8374e47a9ec02b1073ecd7aaecfb34cb8d91
Kaki Kagatan <kkagatan1@my.bcit.ca>
2020-12-03 07:10:13 -0800

    added lobby javascript and changed some layout

---
+++ scripts/Lobby.js
@@ -0,0 +1,40 @@
+const lobbyList = document.querySelector('#Lobby-details');
+
+function renderLobby(doc) {
+    let li = document.createElement('p');
+    let Name = document.createElement('h5');
+    let Game = document.createElement('p');
+    let Code = document.createElement('p');
+    let Players = document.createElement('p');
+    let cross = document.createElement('div');
+
+    li.setAttribute('data-id', doc.id);
+    Name.textContent = doc.data().Name;
+    Game.textContent = doc.data().Game;
+    Code.textContent = doc.data().Code;
+    Players.textContent = doc.data().Players;
+    cross.textContent = 'x';
+
+    li.append(Name);
+    li.append('Game: ', Game);
+    li.append('Code', Code);
+    li.append('Players Needed: ', Players);
+    li.append(cross);
+
+    list.appendChild(li);
+
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('lobby').doc(id).delete();
+        window.location.replace('lobbylist.html');
+    })
+
+}
+
+db.collection('lobby').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        renderLobby(doc);
+        console.log(doc.data());
+    })
+})
